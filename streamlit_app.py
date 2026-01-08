534c747bdf8ef5f0cb96cd27acb268d803b6a555
mitinisan <40918820+mitinisan@users.noreply.github.com>
2026-01-08 03:57:27 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,4 +127,36 @@ start_time = st.slider(
      format="MM/DD/YY - hh:mm")
 st.write("Start time:", start_time)
 
+st.header('st.select_slider')
+
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
+
+start_color, end_color = st.select_slider(
+    "Select a range of color wavelength",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+    value=("red", "blue"),
+)
+st.write("You selected wavelengths between", start_color, "and", end_color)
+
+
 st.header('END OF FILE')
